e620ba549ab2b88f1f3c92d104d7f269af1d2150
Pete Hering <pjhering@gmail.com>
2018-12-29 07:53:15 -0500

    finished drop command

--- main/sources/org/petehering/txtadv/impl/Drop.java
+++ main/sources/org/petehering/txtadv/impl/Drop.java
@@ -3,11 +3,68 @@ package org.petehering.txtadv.impl;
 import org.petehering.txtadv.Command;
 import org.petehering.txtadv.Model;
 
-public class Drop implements Command // TODO: implement this command
+import java.util.HashSet;
+import java.util.Set;
+import org.petehering.txtadv.Command;
+import org.petehering.txtadv.Item;
+import org.petehering.txtadv.Model;
+import org.petehering.txtadv.Player;
+import org.petehering.txtadv.Room;
+
+public class Drop implements Command
 {
     @Override
     public String execute (Model model, String[] args)
     {
-        return getClass().getSimpleName() + " not implemented";
+        if(args.length < 2)
+        {
+            return "drop what?";
+        }
+
+        Player p = model.getPlayer();
+        Room r = model.getRooms().get(p.getLocation());
+        Set<Item> inventory = p.getInventory();
+        Set<Item> dropped = new HashSet<>();
+
+        OUTER:
+        for(int i = 1; i < args.length; i++)
+        {
+            INNER:
+            for(Item item : inventory)
+            {
+                if(item.getName().equals(args[i])) // found a match
+                {
+                    if(!dropped.contains(item)) // not already dropped
+                    {
+                        dropped.add(item);
+                        break INNER;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder()
+            .append("you dropped:\n");
+
+        if(dropped.isEmpty())
+        {
+            sb.append("  nothing\n");
+        }
+        else
+        {
+            Set<Item> contents = r.getContents();
+
+            for(Item it : dropped)
+            {
+                contents.add(it);
+                inventory.remove(it);
+
+                sb.append("  ")
+                  .append(it.getName())
+                  .append("\n");
+            }
+        }
+
+        return sb.toString();
     }
 }
