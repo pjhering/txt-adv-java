2b898a8d3bd1e7e1db204ee5b5812a30ca99f83d
Pete Hering <pjhering@gmail.com>
2018-12-29 10:13:56 -0500

    finished save command

--- main/sources/org/petehering/txtadv/impl/Save.java
+++ main/sources/org/petehering/txtadv/impl/Save.java
@@ -1,13 +1,39 @@
 package org.petehering.txtadv.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.File;
+import java.io.FileWriter;
 import org.petehering.txtadv.Command;
 import org.petehering.txtadv.Model;
 
-public class Save implements Command // TODO: implement this command
+public class Save implements Command
 {
     @Override
     public String execute (Model model, String[] args)
     {
-        return getClass().getSimpleName() + " not implemented";
+        if(args.length < 2)
+        {
+            return "you need to specify the file";
+        }
+
+        File file = new File(args[1]);
+
+        try(FileWriter writer = new FileWriter(file))
+        {
+            Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .serializeNulls()
+                .create();
+            String json = gson.toJson(model);
+            writer.write(json);
+            writer.flush();
+
+            return "saved game in " + args[1];
+        }
+        catch(Exception ex)
+        {
+            return "ERROR: " + ex.toString();
+        }
     }
 }
