d493d69763530b1b5de1dd8d156d643e89359670
Pete Hering <pjhering@gmail.com>
2018-12-30 07:10:46 -0500

    refactored Drop to return List<String> from execute method

--- main/sources/org/petehering/txtadv/impl/Drop.java
+++ main/sources/org/petehering/txtadv/impl/Drop.java
@@ -1,9 +1,8 @@
 package org.petehering.txtadv.impl;
 
-import org.petehering.txtadv.Command;
-import org.petehering.txtadv.Model;
-
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.petehering.txtadv.Command;
 import org.petehering.txtadv.Item;
@@ -11,60 +10,61 @@ import org.petehering.txtadv.Model;
 import org.petehering.txtadv.Player;
 import org.petehering.txtadv.Room;
 
-public class Drop implements Command // TODO: UI_Agnostic
+public class Drop implements Command
 {
     @Override
-    public String execute (Model model, String[] args)
+    public List<String> execute (Model model, String[] args)
     {
+        List<String> response = new ArrayList<>();
+
         if(args.length < 2)
         {
-            return "drop what?";
+            response.add("drop what?");
         }
-
-        Player p = model.getPlayer();
-        Room r = model.getRooms().get(p.getLocation());
-        Set<Item> inventory = p.getInventory();
-        Set<Item> dropped = new HashSet<>();
-
-        OUTER:
-        for(int i = 1; i < args.length; i++)
+        else
         {
-            INNER:
-            for(Item item : inventory)
+            Player p = model.getPlayer();
+            Room r = model.getRooms().get(p.getLocation());
+            Set<Item> inventory = p.getInventory();
+            Set<Item> dropped = new HashSet<>();
+
+            OUTER:
+            for(int i = 1; i < args.length; i++)
             {
-                if(item.getName().equals(args[i])) // found a match
+                INNER:
+                for(Item item : inventory)
                 {
-                    if(!dropped.contains(item)) // not already dropped
+                    if(item.getName().equals(args[i])) // found a match
                     {
-                        dropped.add(item);
-                        break INNER;
+                        if(!dropped.contains(item)) // not already dropped
+                        {
+                            dropped.add(item);
+                            break INNER;
+                        }
                     }
+                }
+            }
+
+            response.add("you dropped:");
+
+            if(dropped.isEmpty())
+            {
+                response.add("  nothing");
+            }
+            else
+            {
+                Set<Item> contents = r.getContents();
+
+                for(Item it : dropped)
+                {
+                    contents.add(it);
+                    inventory.remove(it);
+
+                    response.add("  " + it.getName());
                 }
             }
         }
 
-        StringBuilder sb = new StringBuilder()
-            .append("you dropped:\n");
-
-        if(dropped.isEmpty())
-        {
-            sb.append("  nothing\n");
-        }
-        else
-        {
-            Set<Item> contents = r.getContents();
-
-            for(Item it : dropped)
-            {
-                contents.add(it);
-                inventory.remove(it);
-
-                sb.append("  ")
-                  .append(it.getName())
-                  .append("\n");
-            }
-        }
-
-        return sb.toString();
+        return response;
     }
 }
