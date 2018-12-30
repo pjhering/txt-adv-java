907681f2b8ebafed36078a0026cae77e13ad5124
Pete Hering <pjhering@gmail.com>
2018-12-30 08:01:01 -0500

    refactored Save to return List<String> from execute method

--- main/sources/org/petehering/txtadv/impl/Save.java
+++ main/sources/org/petehering/txtadv/impl/Save.java
@@ -4,36 +4,44 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 import org.petehering.txtadv.Command;
 import org.petehering.txtadv.Model;
 
-public class Save implements Command // TODO: UI_Agnostic
+public class Save implements Command
 {
     @Override
-    public String execute (Model model, String[] args)
+    public List<String> execute (Model model, String[] args)
     {
+        List<String> response = new ArrayList<>();
+
         if(args.length < 2)
         {
-            return "you need to specify the file";
+            response.add("you need to specify the file");
         }
-
-        File file = new File(args[1]);
-
-        try(FileWriter writer = new FileWriter(file))
+        else
         {
-            Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .serializeNulls()
-                .create();
-            String json = gson.toJson(model);
-            writer.write(json);
-            writer.flush();
+            File file = new File(args[1]);
 
-            return "saved game in " + args[1];
+            try(FileWriter writer = new FileWriter(file))
+            {
+                Gson gson = new GsonBuilder()
+                    .setPrettyPrinting()
+                    .serializeNulls()
+                    .create();
+                String json = gson.toJson(model);
+                writer.write(json);
+                writer.flush();
+
+                response.add("saved game in " + args[1]);
+            }
+            catch(Exception ex)
+            {
+                response.add("ERROR: " + ex.toString());
+            }
         }
-        catch(Exception ex)
-        {
-            return "ERROR: " + ex.toString();
-        }
+
+        return response;
     }
 }
