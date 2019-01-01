49ac82c956e7b4081c54a4ec0cc5eeb28762224e
Pete Hering <pjhering@gmail.com>
2019-01-01 09:46:59 -0500

    compiles and runs

--- main/sources/org/petehering/dungeon/Main.java
+++ main/sources/org/petehering/dungeon/Main.java
@@ -1,11 +1,12 @@
+package org.petehering.dungeon;
 
 import com.google.gson.Gson;
 import static java.lang.System.exit;
 import static java.lang.System.out;
 import java.util.List;
 import java.io.Console;
-import java.io.File;
-import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import org.petehering.txtadv.Engine;
 import org.petehering.txtadv.Model;
 import org.petehering.txtadv.Parser;
@@ -14,45 +15,40 @@ public class Main
 {
     public static void main(String[] args)
     {
-        if(args.length != 1)
-        {
-            out.println("USAGE:\n\tjava -jar TxtAdv.jar path/to/model.json");
-            exit(0);
-        }
-        else
-        {
-            Model model;
-            File file = new File(args[0]);
+        Model model;
+        URL url = Main.class.getResource("/dungeon.json");
+        out.println(url);
 
-            try(FileReader reader = new FileReader(file))
+        if(url == null) exit(1);
+
+        try(InputStreamReader reader = new InputStreamReader(url.openStream()))
+        {
+            Parser parser = new Parser(reader);
+            model = parser.getModel();
+
+            Engine engine = new Engine(model);
+            Console console = System.console();
+
+            out.println(model.getTitle());
+            out.println(model.getAuthor());
+            out.println(model.getPreamble());
+            out.println();
+
+            while(!model.getQuit())
             {
-                Parser parser = new Parser(reader);
-                model = parser.getModel();
-
-                Engine engine = new Engine(model);
-                Console console = System.console();
-
-                out.println(model.getTitle());
-                out.println(model.getAuthor());
-                out.println(model.getPreamble());
-                out.println();
-
-                while(!model.getQuit())
+                out.print(">");
+                String request = console.readLine();
+                List<String> response = engine.handle(request);
+                for(String line : response)
                 {
-                    out.print(">");
-                    String request = console.readLine();
-                    List<String> response = engine.handle(request);
-                    for(String line : response)
-                    {
-                        out.println(line);
-                    }
-                    out.println();
+                    out.println(line);
                 }
+                out.println();
             }
-            catch(Exception ex)
-            {
-                ex.printStackTrace();
-            }
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
         }
     }
 }
